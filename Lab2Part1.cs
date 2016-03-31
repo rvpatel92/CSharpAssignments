76abee21d334eeecc62a13b44369d161714c0683
Ravi Patel <ravipatel@Ravis-MacBook-Pro.local>
2016-03-31 13:17:24 -0400

    Here is the first part to lab 2.

---
+++ Lab2Part1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab2RaviPatel
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Course crse1 = new Course ();
+			Console.WriteLine(crse1.ToString ());
+			Console.WriteLine ();
+			Console.WriteLine ();
+			Course crse2 = new Course ("This course teaches you about the importance of C#\nand its awesomeness",
+				               1101, 3, "CSE");
+			Console.WriteLine(crse2.ToString ());
+		}
+	}
+}
